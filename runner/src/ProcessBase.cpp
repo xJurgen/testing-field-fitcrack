3c8957294e42e409e6ed725447af164070581ba4
Fitcrack <fitcrack@fitcrack.local>
2019-11-08 14:27:23 +0100

    up7

--- runner/src/ProcessBase.cpp
+++ runner/src/ProcessBase.cpp
@@ -116,7 +116,7 @@ void ProcessBase::initInPipe(){
 }
 
 void ProcessBase::setInPipe(PipeBase *in_pipe){
-  puts("SetInPipe");
+  printf("SetInPipe %p\n", (void *)in_pipe);
   this->in_pipe_ = in_pipe;
 }
 
