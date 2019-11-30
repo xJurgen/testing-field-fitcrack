8eccb179f6f625f3861f174cc741e601952150e3
Dávid Bolvanský <david.bolvansky@gmail.com>
2019-11-30 19:14:55 +0100

    up

--- runner/src/ProcessWindows.cpp
+++ runner/src/ProcessWindows.cpp
@@ -37,8 +37,10 @@ CloseHandle(process_information_.hProcess);
 CloseHandle(process_information_.hThread);
 
 /* Parent doesn't write */
-err_pipe_->closeAll();
+err_pipe_->closeWrite();
 out_pipe_->closeWrite();
+if (isHashCatProcess) 
+  in_pipe_->closeRead();
 //if (in_pipe_) in_pipe_->closeRead();
 }
 /* Public */
